47f334fbf233f57068f08f5b5d62c179ecc53b0d
Mo0rBy <will.moorby2@gmail.com>
2023-04-15 18:11:15 +0100

    Moved segment angles data into Key objects in musicKeys array. I think it will be best to have all the data required in those objects. Currently adding a 'selected' boolean so only 1 segment will show at a time

--- src/MusicKeys.js
+++ src/MusicKeys.js
@@ -1,126 +1,235 @@
-const C_Major = {
-    chord1: "C Major",
-    chord2: "D minor",
-    chord3: "E minor",
-    chord4: "F Major",
-    chord5: "G Major",
-    chord6: "A minor",
-    chord7: "B diminished"
-}
+// const C_Major = {
+//     chord1: "C Major",
+//     chord2: "D minor",
+//     chord3: "E minor",
+//     chord4: "F Major",
+//     chord5: "G Major",
+//     chord6: "A minor",
+//     chord7: "B diminished"
+// }
 
-const G_Major = {
-    chord1: "G Major",
-    chord2: "A minor",
-    chord3: "B minor",
-    chord4: "C Major",
-    chord5: "D Major",
-    chord6: "E minor",
-    chord7: "F# diminished"
-}
+// const G_Major = {
+//     chord1: "G Major",
+//     chord2: "A minor",
+//     chord3: "B minor",
+//     chord4: "C Major",
+//     chord5: "D Major",
+//     chord6: "E minor",
+//     chord7: "F# diminished"
+// }
 
-const D_Major = {
-    chord1: "D Major",
-    chord2: "E minor",
-    chord3: "F# minor",
-    chord4: "G Major",
-    chord5: "A Major",
-    chord6: "B minor",
-    chord7: "C# diminished"
-}
+// const D_Major = {
+//     chord1: "D Major",
+//     chord2: "E minor",
+//     chord3: "F# minor",
+//     chord4: "G Major",
+//     chord5: "A Major",
+//     chord6: "B minor",
+//     chord7: "C# diminished"
+// }
 
-const A_Major = {
-    chord1: "A Major",
-    chord2: "B minor",
-    chord3: "C# minor",
-    chord4: "D Major",
-    chord5: "E Major",
-    chord6: "F# minor",
-    chord7: "G# diminished"
-}
+// const A_Major = {
+//     chord1: "A Major",
+//     chord2: "B minor",
+//     chord3: "C# minor",
+//     chord4: "D Major",
+//     chord5: "E Major",
+//     chord6: "F# minor",
+//     chord7: "G# diminished"
+// }
 
-const E_Major = {
-    chord1: "E Major",
-    chord2: "F# minor",
-    chord3: "G# minor",
-    chord4: "A Major",
-    chord5: "B Major",
-    chord6: "C# minor",
-    chord7: "D# diminished"
-}
+// const E_Major = {
+//     chord1: "E Major",
+//     chord2: "F# minor",
+//     chord3: "G# minor",
+//     chord4: "A Major",
+//     chord5: "B Major",
+//     chord6: "C# minor",
+//     chord7: "D# diminished"
+// }
 
-const B_Major = {
-    chord1: "B Major",
-    chord2: "C# minor",
-    chord3: "D# minor",
-    chord4: "E Major",
-    chord5: "F# Major",
-    chord6: "G# minor",
-    chord7: "A# diminished"
-}
+// const B_Major = {
+//     chord1: "B Major",
+//     chord2: "C# minor",
+//     chord3: "D# minor",
+//     chord4: "E Major",
+//     chord5: "F# Major",
+//     chord6: "G# minor",
+//     chord7: "A# diminished"
+// }
 
-const F_sharp_Major = {
-    chord1: "F# Major",
-    chord2: "G# minor",
-    chord3: "A# minor",
-    chord4: "B Major",
-    chord5: "C# Major",
-    chord6: "D# minor",
-    chord7: "E# diminished"
-}
+// const F_sharp_Major = {
+//     chord1: "F# Major",
+//     chord2: "G# minor",
+//     chord3: "A# minor",
+//     chord4: "B Major",
+//     chord5: "C# Major",
+//     chord6: "D# minor",
+//     chord7: "E# diminished"
+// }
 
-const C_sharp_Major = {
-    chord1: "C# Major",
-    chord2: "D# minor",
-    chord3: "E# minor",
-    chord4: "F# Major",
-    chord5: "G# Major",
-    chord6: "A# minor",
-    chord7: "B# diminished"
-}
+// const C_sharp_Major = {
+//     chord1: "C# Major",
+//     chord2: "D# minor",
+//     chord3: "E# minor",
+//     chord4: "F# Major",
+//     chord5: "G# Major",
+//     chord6: "A# minor",
+//     chord7: "B# diminished"
+// }
 
-const A_flat_Major = {
-    chord1: "A♭ Major",
-    chord2: "B♭ minor",
-    chord3: "C minor",
-    chord4: "D♭ Major",
-    chord5: "E♭ Major",
-    chord6: "F minor",
-    chord7: "G diminished"
-}
+// const A_flat_Major = {
+//     chord1: "A♭ Major",
+//     chord2: "B♭ minor",
+//     chord3: "C minor",
+//     chord4: "D♭ Major",
+//     chord5: "E♭ Major",
+//     chord6: "F minor",
+//     chord7: "G diminished"
+// }
 
-const E_flat_Major = {
-    chord1: "E♭ Major",
-    chord2: "F minor",
-    chord3: "G minor",
-    chord4: "A♭ Major",
-    chord5: "B♭ Major",
-    chord6: "C minor",
-    chord7: "D diminished"
-}
+// const E_flat_Major = {
+//     chord1: "E♭ Major",
+//     chord2: "F minor",
+//     chord3: "G minor",
+//     chord4: "A♭ Major",
+//     chord5: "B♭ Major",
+//     chord6: "C minor",
+//     chord7: "D diminished"
+// }
 
-const B_flat_Major = {
-    chord1: "B♭ Major",
-    chord2: "C minor",
-    chord3: "D minor",
-    chord4: "E♭ Major",
-    chord5: "F Major",
-    chord6: "G minor",
-    chord7: "A diminished"
-}
+// const B_flat_Major = {
+//     chord1: "B♭ Major",
+//     chord2: "C minor",
+//     chord3: "D minor",
+//     chord4: "E♭ Major",
+//     chord5: "F Major",
+//     chord6: "G minor",
+//     chord7: "A diminished"
+// }
 
-const F_Major = {
-    chord1: "F Major",
-    chord2: "G minor",
-    chord3: "A minor",
-    chord4: "B♭ Major",
-    chord5: "C Major",
-    chord6: "D minor",
-    chord7: "E diminished"
-}
+// const F_Major = {
+//     chord1: "F Major",
+//     chord2: "G minor",
+//     chord3: "A minor",
+//     chord4: "B♭ Major",
+//     chord5: "C Major",
+//     chord6: "D minor",
+//     chord7: "E diminished"
+// }
 
 // How to deal with 'alternative' forms of the key?
 // i.e. F# Major = G♭ Major
 // How should enharmonic equivalents be dealth with in general??
+
+/* ---------------------------------------------- - --------------------------------------------- */
+// ^^ Above was the orginal idea for key objects, have now realised I will need metadata of the segments as well
+
+const C_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+  // Don't need this for now
+  // chords: {
+  //   1: "C Major",
+  //   2: "D minor",
+  //   3: "E minor",
+  //   4: "F Major",
+  //   5: "G Major",
+  //   6: "A minor",
+  //   7: "B diminished"
+  // }
+}
+
+const G_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const D_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const A_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const E_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const B_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const F_sharp_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const C_sharp_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const A_flat_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const E_flat_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const B_flat_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
+
+const F_Major = {
+  segmentMetadata: {
+    startAngle: null,
+    endAngle: null,
+    selected: null
+  }
+}
 
 const musicKeys = [
     C_Major,
@@ -137,6 +246,18 @@ const musicKeys = [
     F_Major
 ]
 
-// export default musicKeys;
+// Generate segment metadata
+// Adds the startAngle and endAngle of each segment to an array
+// Uses cumulativeAngle to keep track of each segment
+const sectorAngle = (Math.PI * 2) / 12;
+var cumulativeAngle = 0;
+for (let i = 0; i < musicKeys.length; i++) {
+  let startAngle = cumulativeAngle;
+  cumulativeAngle += sectorAngle;
+  let endAngle = cumulativeAngle;
+  musicKeys[i].segmentMetadata.startAngle = startAngle;
+  musicKeys[i].segmentMetadata.endAngle = endAngle;
+}
 
-export { C_Major, G_Major };
+
+export default musicKeys;
--- src/components/CircleOfFifths/CircleOfFifths.js
+++ src/components/CircleOfFifths/CircleOfFifths.js
@@ -1,46 +1,33 @@
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import { C_Major, G_Major } from '../../MusicKeys';
+import musicKeys from '../../MusicKeys'
 
 // export default function CircleOfFifths({ outerRadius, numSectors }) {
 export default function CircleOfFifths() {
 
   const outerRadius = 400;
-  const numSectors = 12;
 
   // const diameter = outerRadius * 2;
   const diameter = 2000;
   const innerRadius = outerRadius * 0.6;
-  const sectorAngle = (Math.PI * 2) / numSectors
-  // const data = Object.values(C_Major);
   const data = ["C", "G", "D", "A", "E", "B", "F#", "C#", "A♭", "E♭", "B♭", "F"]
-  // const data = [25, 50, 30, 46];
-  var angles = [];
 
   const svgRef = useRef();
 
-  // Adds the startAngle and endAngle of each segment to an array
-  // Uses cumulativeAngle to keep track of each segment
-  var cumulativeAngle = 0;
-  for (let i =0; i<data.length; i++) {
-    let startAngle = cumulativeAngle;
-    cumulativeAngle += sectorAngle;
-    let endAngle = cumulativeAngle;
-    angles.push({startAngle, endAngle});
-  }
+  console.log(musicKeys)
 
   const arcGenerator = d3.arc()
-                      .innerRadius(innerRadius)
-                      .outerRadius(outerRadius);
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius);
 
   useEffect(() => {
     const circleSegments = d3.selectAll('.circle-segment');
 
     circleSegments
-      .data(angles)
-      .attr('d', function(angle){ 
+      .data(musicKeys)
+      .attr('d', function(key){ 
         return arcGenerator(
-          {startAngle: angle.startAngle, endAngle: angle.endAngle}
+          {startAngle: key.segmentMetadata.startAngle, endAngle: key.segmentMetadata.endAngle}
         );}
       );
   }, [])
@@ -55,14 +42,12 @@ export default function CircleOfFifths() {
     <svg ref={svgRef}
       height={diameter}
       width={diameter}>
-        <g transform='translate(400,400)'>
-          {angles.map(angle => <path className='circle-segment' stroke='red'/>)}
+      <g className='circle-container' transform='translate(400,400)'>
+        <circle r={outerRadius} className="base-circle" />
+        <g className='circle-segments-container' transform='rotate(-15)'>
+          {musicKeys.map(key => <path className='circle-segment' stroke='red'/>)}
         </g>
-      {/* <circle
-        r={radius}
-        cx={radius} cy={radius}
-        className="base-circle">
-      </circle> */}
+      </g>
     </svg>
   )
 }
