4fb31a9c6aa0b4ee9631a85c7434f9afca2b0206
Mo0rBy <will.moorby2@gmail.com>
2023-05-07 15:31:10 +0100

    Refactored segmentMetadata.selected to visible and added some styling

--- src/components/CircleOfFifths/CircleOfFifths.js
+++ src/components/CircleOfFifths/CircleOfFifths.js
@@ -1,6 +1,6 @@
+import * as d3 from 'd3';
 import { useEffect, useState } from 'react';
 import musicKeys from '../../MusicKeys';
-import * as d3 from 'd3';
 
 // export default function CircleOfFifths({ outerRadius, numSectors }) {
 export default function CircleOfFifths() {
@@ -16,17 +16,17 @@ export default function CircleOfFifths() {
   useEffect(() => {
     let newMusicKeysObject = musicKeysObject;
     for (let i=0; i<newMusicKeysObject.length; i++) {
-      newMusicKeysObject[i].segmentMetadata.selected = true;
+      newMusicKeysObject[i].segmentMetadata.visible = true;
     }
     setMusicKeysObject([...newMusicKeysObject]);
   }, [])
 
   const onMouseUpHandler = (index) => {
     let newMusicKeysObject = musicKeysObject;
-    if (newMusicKeysObject[index].segmentMetadata.selected) {
-      newMusicKeysObject[index].segmentMetadata.selected = false;
+    if (newMusicKeysObject[index].segmentMetadata.visible) {
+      newMusicKeysObject[index].segmentMetadata.visible = false;
     } else {
-      newMusicKeysObject[index].segmentMetadata.selected = true;
+      newMusicKeysObject[index].segmentMetadata.visible = true;
     }
     setMusicKeysObject([...newMusicKeysObject]);
   }
@@ -42,14 +42,13 @@ export default function CircleOfFifths() {
   const renderSegment = (musicKey, index) => {
     let arc = calculateArc(musicKey.segmentMetadata.startAngle, musicKey.segmentMetadata.endAngle);
     let [arcCenterX, arcCenterY] = arc.centroid();
-    if (musicKey.segmentMetadata.selected === true) {
-      return <g key={index} onMouseUp={() => onMouseUpHandler(index)}>
+    if (musicKey.segmentMetadata.visible === true) {
+      return <g className='circle-segment' key={index} onMouseUp={() => onMouseUpHandler(index)}>
               <path 
                 d={arc.apply()} // apply() is needed to generate the string that goes into the 'd' attribute
-                stroke='red'
-                fillOpacity={0.05}
               />
-              <text x={arcCenterX} y={arcCenterY} rotate={15}>
+              {/* Need to adjust center X position due to increased font-size */}
+              <text x={arcCenterX-10} y={arcCenterY} rotate={15}> 
                 {musicKey.chords[0].replace('Major', '')}
               </text>
             </g>
@@ -61,8 +60,8 @@ export default function CircleOfFifths() {
       <svg
         height={diameter}
         width={diameter}>
-        <g className='circle-container' transform='translate(400,400)'>
-          <circle r={outerRadius} className="base-circle" fillOpacity={0.1}/>
+        <g className='circle-container' transform={`translate(${diameter/2},${diameter/2})`}>
+          <circle r={outerRadius} className="base-circle"/>
           <g className='circle-segments-container' transform='rotate(-15)'>
             {musicKeysObject.map((musicKey, index) => renderSegment(musicKey, index))}
           </g>
